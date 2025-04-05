5b74904053eed94188c40c187e1001f8561586be
huihui <1659318900@qq.com>
2025-04-05 17:27:34 +0800

    调整目录结构

---
+++ eslint.config.ts
@@ -0,0 +1,35 @@
+import { globalIgnores } from 'eslint/config'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  skipFormatting,
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'no-undef': 'error',
+      // 'vue/multi-word-component-names': [
+      //   'warn',
+      //   {
+      //     ignores: ['index'], // vue组件名称多单词组成（忽略index.vue）
+      //   },
+      // ],
+      // 'vue/no-setup-props-destructure
+    },
+  },
+)
